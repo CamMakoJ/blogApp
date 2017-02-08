8876f875e6aa45023e2a658b7fc0c3a91e80e200
Cameron Johnston <Cameron Johnston>
2017-02-07 23:23:07 -0700

    created navbar and made all views children of it

--- router/router.js
+++ router/router.js
@@ -2,36 +2,61 @@ angular.module('blogApp')
 
     .config(["$stateProvider", "$urlRouterProvider", function($stateProvider, $urlRouterProvider) {
 
-        $stateProvider
+            $stateProvider
+                //NavBar
+                .state(navbarState = {
+                    name: 'root',
+                    url: '',
+                    abstract: true,
+                    views: {
+                        'navbar': {
+                            templateUrl: 'views/navbar/navbar.html'
+                        }
+                    }
+                })
 
-            .state(homeState = {
-                name: 'home',
-                url: '/',
-                templateUrl: 'views/home/home.html',
-            })
+                //General Pages
+                .state(homeState = {
+                    name: 'root.home',
+                    url: '/',
+                    views: {
+                        'main@': {
+                            templateUrl: 'views/home/home.html'
+                        }
+                    }
+                })
 
-            .state(signInState = {
-                name: 'login',
-                url: '/login',
-                templateUrl: 'views/login/login.html',
-            })
+                .state(signInState = {
+                    name: 'root.login',
+                    url: '/login',
+                    views: {
+                        'main@': {
+                            templateUrl: 'views/login/login.html'
+                        }
+                    }
+                })
 
-            .state(adminState = {
-                name: 'admin',
-                url: '/admin',
-                templateUrl: 'views/admin/admin.html',
-                resolve: {
-                    // controller will not be loaded until $requireSignIn resolves
-                    // Auth refers to our $firebaseAuth wrapper in the factory below
-                    "currentAuth": ["Auth", function(Auth) {
-                        // $requireSignIn returns a promise so the resolve waits for it to complete
-                        // If the promise is rejected, it will throw a $stateChangeError (see above)
-                        return Auth.$requireSignIn();
-                    }]
-                }
-            });
+                //Pages requireSignIn
+                .state(adminState = {
+                        name: 'root.admin',
+                        url: '/admin',
+                        views: {
+                            'main@': {
+                                templateUrl: 'views/admin/admin.html'
+                            }
+                          },
+                            resolve: {
+                                // controller will not be loaded until $requireSignIn resolves
+                                // Auth refers to our $firebaseAuth wrapper in the factory below
+                                "currentAuth": ["Auth", function(Auth) {
+                                    // $requireSignIn returns a promise so the resolve waits for it to complete
+                                    // If the promise is rejected, it will throw a $stateChangeError (see above)
+                                    return Auth.$requireSignIn();
+                                }]
+                            }
+                        });
 
-        // Send to the homepage if the URL was not found
-        $urlRouterProvider.otherwise("/");
+                    // Send to the homepage if the URL was not found
+                    $urlRouterProvider.otherwise("/");
 
-    }]);
+                }]);
