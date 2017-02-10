2255f542c47ff1ad4ee00cd6ce2f5075ec5456d8
Cameron Johnston <Cameron Johnston>
2017-02-10 16:55:16 -0700

    updated theme and added blog post cards

--- public/app.js
+++ public/app.js
@@ -1,13 +1,16 @@
 angular.module('blogApp', ['ui.router', 'firebase', 'ngMaterial'])
 
-.run(["$rootScope", "$state", function($rootScope, $state) {
-  $rootScope.$on("$stateChangeError", function(event, toState, toParams, fromState, fromParams, error) {
-    // We can catch the error thrown when the $requireSignIn promise is rejected
-    // and redirect the user back to the home page
-    if (error === "AUTH_REQUIRED") {
-      $state.go("home");
-    }
-  });
-}])
+    .run(["$rootScope", "$state", function($rootScope, $state) {
+        $rootScope.$on("$stateChangeError", function(event, toState, toParams, fromState, fromParams, error) {
+            // We can catch the error thrown when the $requireSignIn promise is rejected
+            // and redirect the user back to the home page
+            if (error === "AUTH_REQUIRED") {
+                $state.go("home");
+            }
+        });
+    }])
 
-;
+    .config(function($mdThemingProvider) {
+            $mdThemingProvider.theme('default')
+                .dark();
+        });
